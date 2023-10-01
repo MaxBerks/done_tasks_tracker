c6b4f5605bdf1f60c969593781d07e4f5854e921
Maksym Berkovskyi <maksym.berkovskyi@student.tuke.sk>
2023-10-01 08:53:54 +0200

    rewrite Header component to TypeScript

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -2,7 +2,15 @@ import React from "react"
 import "./Header.scss"
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io"
 
-export default function Header({ currentMoment, onPrevMonth, onNextMonth }) {
+type TypeOnChangeMonthFunction = () => void
+
+type TypeHeaderProps = {
+	currentMoment: any //!!!!!!!!!!!!!!!!!!!!!!
+	onPrevMonth: TypeOnChangeMonthFunction
+	onNextMonth: TypeOnChangeMonthFunction
+}
+
+export default function Header({ currentMoment, onPrevMonth, onNextMonth }: TypeHeaderProps) {
 	return (
 		<div className="header">
 			<div className="header__wrapper">
@@ -10,14 +18,12 @@ export default function Header({ currentMoment, onPrevMonth, onNextMonth }) {
 					className="header__arrowBtn header__arrowBtn-left"
 					onClick={() => onPrevMonth()}
 				/>
-				<h1 className="header__date">
-					{currentMoment.format("MMM YYYY")}
-				</h1>
+				<h1 className="header__date">{currentMoment.format("MMM YYYY")}</h1>
 				<IoIosArrowForward
 					className="header__arrowBtn header__arrowBtn-right"
 					onClick={() => onNextMonth()}
 				/>
 			</div>
 		</div>
-	)	
+	)
 }
